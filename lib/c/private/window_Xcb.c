f3dda0f570ac1965ddb0bb4c99e21e0f0f8fc29e
Julian Kirsch <contact@juliankirsch.me>
2020-06-11 01:03:34 +0200

    Added support for most events
    Mouse wheel event seems to be broken, though.
    Additionally, the key press/release events do
    not translate keys yet.

--- lib/c/private/window_Xcb.c
+++ lib/c/private/window_Xcb.c
@@ -1,15 +1,48 @@
 #include "window_Xcb.h"
 #include "window_common.h"
+#include "input_common.h"
 #include <fundament/event.h> 
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <xcb/xcb.h>
 
 static xcb_connection_t* connection = NULL;
 static xcb_screen_t* screen = NULL;
 static xcb_atom_t atom_protocols = 0;
 static xcb_atom_t atom_delete_window = 0;
+
+// This atom is used to attach the window id,
+// which the again is used to by the event handler
+// to determine the exact window.
+static xcb_atom_t atom_fundament_id = 0;
+
+static uint32_t get_fundament_id_from_window(fn_native_window_handle_t handle) {
+    xcb_get_property_cookie_t id_cookie = xcb_get_property(
+        connection,
+        false,
+        handle,
+        atom_fundament_id,
+        XCB_ATOM_INTEGER,
+        0,
+        1
+    );
+
+    xcb_get_property_reply_t* reply = xcb_get_property_reply(
+        connection,
+        id_cookie,
+        NULL
+    );
+
+    xcb_flush(connection);
+
+    uint32_t idx = *(uint32_t*) xcb_get_property_value(reply);
+    xcb_flush(connection);
+    free(reply); 
+
+    return idx;
+}
 
 void fn__imp_init_window_context() {
     connection = xcb_connect(NULL, NULL);
@@ -41,18 +74,42 @@ void fn__imp_init_window_context() {
         0
     );
 
+    xcb_intern_atom_cookie_t id_cookie = xcb_intern_atom(
+        connection,
+        0,
+        12,
+        "FUNDAMENT_ID" 
+    );
+
+    xcb_intern_atom_reply_t* id_cookie_reply = xcb_intern_atom_reply(
+        connection,
+        id_cookie,
+        0
+    );
+
     xcb_flush(connection);
 
     atom_protocols = protocols_reply->atom;
     atom_delete_window = delete_window_reply->atom;
+    atom_fundament_id = id_cookie_reply->atom; 
     
     free(protocols_reply);
     free(delete_window_reply);
+    free(id_cookie_reply);
 }
 
 fn_native_window_handle_t fn__imp_create_window(uint32_t index) {
     fn_native_window_handle_t handle = xcb_generate_id(connection); 
     
+    const uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+    const uint32_t mask_values[] = {
+        screen->white_pixel,
+        XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS
+        | XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_POINTER_MOTION
+        | XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE
+        | XCB_EVENT_MASK_STRUCTURE_NOTIFY
+    };
+
     xcb_create_window(
         connection,
         XCB_COPY_FROM_PARENT,
@@ -65,8 +122,8 @@ fn_native_window_handle_t fn__imp_create_window(uint32_t index) {
         10,
         XCB_WINDOW_CLASS_INPUT_OUTPUT,
         screen->root_visual,
-        0,
-        NULL
+        mask,
+        mask_values
     );
 
     xcb_change_property(
@@ -79,26 +136,34 @@ fn_native_window_handle_t fn__imp_create_window(uint32_t index) {
         1,
         &atom_delete_window
     );
-    
-    xcb_map_window(connection, handle);
+   
+    xcb_change_property(
+        connection,
+        XCB_PROP_MODE_REPLACE,
+        handle,
+        atom_fundament_id,
+        XCB_ATOM_INTEGER,
+        32,
+        1,
+        &index
+    );
 
+    xcb_map_window(connection, handle);
     xcb_flush(connection);
 
     return handle;
 }
 
 void fn__imp_destroy_window(fn_native_window_handle_t handle) {
-    xcb_destroy_window(handle);
+    xcb_destroy_window(connection, handle);
     xcb_flush(connection);
 }
-
 
 void fn__imp_window_set_size(
     fn_native_window_handle_t handle,
     uint32_t width,
     uint32_t height
 ) {
-
     const uint32_t values[] = {
         width,
         height
@@ -113,7 +178,6 @@ void fn__imp_window_set_size(
     );
 
     xcb_flush(connection);
-
 }
 
 void fn__imp_window_set_title(
@@ -166,23 +230,158 @@ void fn__imp_window_set_visibility(
     xcb_flush(connection);
 }
 
-
 void fn__imp_window_poll_events() {
     xcb_generic_event_t* ev = NULL;
     struct fn_event fev = {0, };
     
     while((ev = xcb_poll_for_event(connection))) {
         switch(ev->response_type & ~0x80) {
+
             case XCB_CLIENT_MESSAGE: {
                 xcb_client_message_event_t* cev =
                     (xcb_client_message_event_t*) ev;
 
+                uint32_t idx = get_fundament_id_from_window(cev->window);
+
                 if(cev->data.data32[0] == atom_delete_window) {
                     fev.type = fn_event_type_closed;
+                    fev.window.id = idx + 1;
                     fn__push_event(&fev);
                 }
+            } break;
+
+            case XCB_CONFIGURE_NOTIFY: {
+                xcb_configure_notify_event_t* cev =
+                    (xcb_configure_notify_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->window);
+                struct fn__window* win = &fn__g_window_context.windows[idx];
+
+                if(win->width == cev->width && win->height == cev->height)
+                    break;
+
+                win->width = cev->width;
+                win->height = cev->height;
+
+                fev.type = fn_event_type_resized;
+                fev.width = win->width;
+                fev.height = win->height;
+
+                fn__push_event(&fev); 
+            } break;
+
+            case XCB_FOCUS_IN: {
+                xcb_focus_in_event_t* cev =
+                    (xcb_focus_in_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event); 
+                fev.type = fn_event_type_focus_gained;
+                fev.window.id = idx + 1;
+                fn__push_event(&fev);
+            } break;
+
+            case XCB_FOCUS_OUT: {
+                xcb_focus_out_event_t* cev =
+                    (xcb_focus_out_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event);
+                fev.type = fn_event_type_focus_lost;
+                fev.window.id = idx + 1;
+                fn__push_event(&fev);
+            } break;
+
+            case XCB_KEY_PRESS: {
+                xcb_key_press_event_t* cev = 
+                    (xcb_key_press_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event);
+                
+                fev.type = fn_event_type_key_pressed;
+                fev.window.id = idx + 1;
+                fn__push_event(&fev);
+            } break;
+
+            case XCB_KEY_RELEASE: {
+                xcb_key_release_event_t* cev =
+                    (xcb_key_release_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event); 
+                
+                fev.type = fn_event_type_key_released;
+                fev.window.id = idx + 1;
+                fn__push_event(&fev);
+            } break;
+
+            case XCB_BUTTON_PRESS: {
+                xcb_button_press_event_t* cev =
+                    (xcb_button_press_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event);
+
+                if((cev->event & XCB_BUTTON_MASK_4)
+                    || cev->event & XCB_BUTTON_MASK_5) {
+                    fev.type = fn_event_type_mouse_wheel;
+                    fev.window.id = idx + 1;
+                    fev.button = fn__get_pressed_buttons();
+                    fev.x = cev->event_x;
+                    fev.y = cev->event_y;
+                    fn__push_event(&fev);
+                    break;
+                }
+
+                if(cev->event & XCB_BUTTON_MASK_1)
+                    fn__set_button_state(fn_button_left, true);
+
+                if(cev->event & XCB_BUTTON_MASK_2)
+                    fn__set_button_state(fn_button_right, true);
+
+                if(cev->event & XCB_BUTTON_MASK_3)
+                    fn__set_button_state(fn_button_middle, true);
                     
-            }
+                fev.type = fn_event_type_button_pressed;
+                fev.window.id = idx + 1;
+                fev.button = fn__get_pressed_buttons();
+                fev.x = cev->event_x;
+                fev.y = cev->event_y;
+                fn__push_event(&fev);
+            } break;
+
+            case XCB_BUTTON_RELEASE: {
+                xcb_button_release_event_t* cev = 
+                    (xcb_button_release_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event);
+
+                if(cev->event & XCB_BUTTON_MASK_1)
+                    fn__set_button_state(fn_button_left, false); 
+
+                if(cev->event & XCB_BUTTON_MASK_2)
+                    fn__set_button_state(fn_button_right, false);
+        
+                if(cev->event & XCB_BUTTON_MASK_3)
+                    fn__set_button_state(fn_button_middle, false);
+
+                fev.type = fn_event_type_button_released;
+                fev.window.id = idx + 1,
+                fev.button = fn__get_pressed_buttons();
+                fev.x = cev->event_x;
+                fev.y = cev->event_y;
+                fn__push_event(&fev);
+            } break;
+
+            case XCB_MOTION_NOTIFY: {
+                xcb_motion_notify_event_t* cev =
+                    (xcb_motion_notify_event_t*) ev;
+
+                uint32_t idx = get_fundament_id_from_window(cev->event);
+
+                fev.type = fn_event_type_mouse_moved;
+                fev.window.id = idx + 1;
+                fev.x = cev->event_x;
+                fev.y = cev->event_y;
+                fn__push_event(&fev);
+            } break;
+ 
             default: break;
         }
 
