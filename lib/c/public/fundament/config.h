ce68932b71bca10064d66685b2f3f5fd4c4130ab
Julian Kirsch <contact@juliankirsch.me>
2020-06-17 18:03:48 +0200

    Using CMake to 'provide' version in config.h

---
+++ lib/c/public/fundament/config.h
@@ -0,0 +1,19 @@
+#ifndef FUNDAMENT_CONFIG_H
+#define FUNDAMENT_CONFIG_H
+
+//=============================================================================
+// The following section defines the libraries metadata; i.e. version, author,
+// etc.
+//=============================================================================
+
+#define FUNDAMENT_VERSION_MAJOR 0
+#define FUNDAMENT_VERSION_MINOR 2
+#define FUNDAMENT_VERSION_PATCH 0
+
+#define FUNDAMENT_PRODUCT_VERSION \
+    FUNDAMENT_VERSION_MAJOR, FUNDAMENT_VERSION_MINOR, FUNDAMENT_VERSION_PATCH
+
+#define FUNDAMENT_PRODUCT_VERSION_STR \
+    #FUNDAMENT_PRODUCT_VERSION
+
+#endif  // FUNDAMENT_CONFIG_H
