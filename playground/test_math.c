f49f9d67d9c64f1d50e388461ced8eacc3c38fd4
Julian Kirsch <contact@juliankirsch.me>
2020-08-14 17:54:09 +0200

    Fixed names in test_math.c

--- playground/test_math.c
+++ playground/test_math.c
@@ -4,13 +4,13 @@
 
 int main() {
     for(float it = 1; it <= 10; ++it) {
-        struct fn_vec3f ex = {it, 0, 0};
-        printf("Len of E_x (vec3f) (%f): %f\n", it, fn_vec3f_len(ex));
+        struct fn_float3 ex = {it, 0, 0};
+        printf("Len of E_x (vec3f) (%f): %f\n", it, fn_float3_len(ex));
     }
 
     {
-        struct fn_vec4f ex = {1, 0, };
-        printf("Len of E_x (vec4f): %f\n", fn_vec4f_len(ex));
+        struct fn_float4 ex = {1, 0, };
+        printf("Len of E_x (vec4f): %f\n", fn_float4_len(ex));
     }
     return 0;
 }
