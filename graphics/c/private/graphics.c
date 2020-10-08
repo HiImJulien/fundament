90ba48fe0f7bcb2f213a4fd605e5751d0ed8f9a7
Julian Kirsch <contact@juliankirsch.me>
2020-10-08 15:41:36 +0200

    Extended API with surfaces, fences and errors.

--- graphics/c/private/graphics.c
+++ graphics/c/private/graphics.c
@@ -1,11 +1,70 @@
 #include <fundament/graphics.h>
 
+#include <assert.h>
+
+//==============================================================================
+// The following section implements internal utility functions.
+//==============================================================================
+
+static void fn__get_device_id(uint32_t id) {
+
+}
+
+static void fn__get_device(struct fn_gfx_device device) {
+    return fn__get_device_id(device.id);
+}
+
+//==============================================================================
+// The following section implements the public API as exposed in 
+// "fundament/graphics.c".
+//==============================================================================
+
+//--------------------------------- DEVICES ----------------------------------//
+
 struct fn_gfx_device fn_create_gfx_device() {
     return (struct fn_gfx_device) { 0 };
 }
 
 void fn_destroy_gfx_device(struct fn_gfx_device device) {
 }
+
+//--------------------------------- SURFACES ---------------------------------//
+
+struct fn_gfx_surface fn_create_gfx_surface(
+    struct fn_gfx_device device,
+    struct fn_gfx_surface_desc* desc) {
+    assert(desc && "'desc' must not be NULL.");
+
+    if(desc->window.id == (uint32_t) desc->native_window 
+        && desc->native_window == 0) {
+        if(desc->error) {
+            desc->error->type = fn_gfx_error_invalid_desc;
+            desc->error->reason = "You must provide a window to create a"
+                                  "surface for.";
+        }
+        return (struct fn_gfx_surface) { 0, };
+    }
+
+
+    return (struct fn_gfx_surface) { 0, };  
+}
+
+void fn_destroy_gfx_surface(struct fn_gfx_surface surface) {
+    
+}
+
+//---------------------------------- FENCES ----------------------------------//
+
+struct fn_gfx_fence fn_create_gfx_fence(
+    struct fn_gfx_device device,
+    struct fn_gfx_fence_desc* desc) {
+    return (struct fn_gfx_fence) { 0, };
+}
+
+void fn_destroy_gfx_fence(struct fn_gfx_fence fence) {
+}
+
+//------------------------------ COMMAND QUEUES ------------------------------//
 
 struct fn_gfx_queue fn_create_gfx_queue(
     struct fn_gfx_device device,
@@ -15,6 +74,8 @@ struct fn_gfx_queue fn_create_gfx_queue(
 
 void fn_destroy_gfx_queue(struct fn_gfx_queue queue) {
 }
+
+//----------------------------- COMMAND BUFFERS ------------------------------//
 
 struct fn_gfx_cmd_buffer fn_create_gfx_cmd_buffer(
     struct fn_gfx_queue queue,
@@ -26,6 +87,8 @@ struct fn_gfx_cmd_buffer fn_create_gfx_cmd_buffer(
 void fn_destroy_gfx_cmd_buffer(struct fn_gfx_cmd_buffer buffer) {
 }
 
+//--------------------------------- BUFFERS ----------------------------------//
+
 struct fn_gfx_buffer fn_create_gfx_buffer(
     struct fn_gfx_device device,
     struct fn_gfx_buffer* desc) {
@@ -34,6 +97,8 @@ struct fn_gfx_buffer fn_create_gfx_buffer(
 
 void fn_destroy_gfx_buffer(struct fn_gfx_buffer buffer) {
 }
+
+//-------------------------------- PIPELINES ---------------------------------//
 
 struct fn_gfx_pipeline fn_create_gfx_pipeline(
     struct fn_gfx_device device,
--- graphics/c/public/fundament/graphics.h
+++ graphics/c/public/fundament/graphics.h
@@ -5,35 +5,32 @@
 //                                  GRAPHICS
 //
 // This module wraps graphics APIs such as Direct3D 12, Metal or Vulkan.
-// The API is choosen based on the target system.
+// The API is choosen based on the target system, for which the library
+// is being compiled for.
+// 
+//                              OBJECT LIFETIME
+// Objects are created using `fn_create_*object*` functions, which accept
+// the parent device (i.e. the GPU to use) and a description of the object
+// to create.
 //
-// All resources are wrapped behind a typesafe numeric handle of the form
-//  struct fn_gfx_*resource* { uint32_t id; };
-//
-// E.g.:
-//  struct fn_gfx_pipeline { uint32_t id; };
-//  struct fn_gfx_buffer{ uint32_t id; };
-//
-// If the allocation fails, the `id` equals to zero.
-// A handle may incorporate additional data. For instance GPU resources, 
-// include the ID of the device they belong to:
-//   struct fn_gfx_buffer{
-//     struct fn_gfx_device parent;
-//     uint32_t             id;
-//   };
+// If the parent device of an object is being released, all child objects are
+// invalidated.
 //
 //==============================================================================
 
 #include <stdbool.h>
 #include <stdint.h>
 
+#include <fundament/window.h>
 
 //==============================================================================
 //                                   ERRORS
 //==============================================================================
 
 enum fn_gfx_error_type {
-    fn_gfx_error_none
+    fn_gfx_error_none,
+    fn_gfx_error_invalid_device,
+    fn_gfx_error_invalid_desc
 };
 
 struct fn_gfx_error {
@@ -52,6 +49,55 @@ struct fn_gfx_device{ uint32_t id; };
 
 struct fn_gfx_device fn_create_gfx_device();
 void fn_destroy_gfx_device(struct fn_gfx_device device);
+
+//==============================================================================
+//                                  SURFACES
+// 
+// Represents the surface of a window to render onto.
+//==============================================================================
+
+struct fn_gfx_surface{ uint32_t device_id; uint32_t id; };
+
+//
+// Describes the surface to create.
+//
+// Note:
+// - Only `window` or `native_window` must be specified.
+//
+struct fn_gfx_surface_desc {
+    struct fn_window            window;
+    fn_native_window_handle_t   native_window;
+    const char*                 label; 
+    struct fn_gfx_error*        error; 
+};
+
+struct fn_gfx_surface fn_create_gfx_surface(
+    struct fn_gfx_device device,
+    struct fn_gfx_surface_desc* desc
+);
+
+void fn_destroy_gfx_surface(struct fn_gfx_surface surface);
+
+//==============================================================================
+//                                   FENCES
+//
+// The GPU works in parallel to the CPU, requiring you to synchronise the two,
+// when updating resources (i.e. constants buffers, etc.).
+//==============================================================================
+
+struct fn_gfx_fence{ uint32_t device_id; uint32_t id; };
+
+struct fn_gfx_fence_desc {
+    const char*             label;
+    struct fn_gfx_error*    error;
+};
+
+struct fn_gfx_fence fn_create_gfx_fence(
+    struct fn_gfx_device device,
+    struct fn_gfx_fence_desc* desc
+);
+
+void fn_destroy_gfx_fence(struct fn_gfx_fence fence);
 
 //==============================================================================
 //                               COMMAND QUEUES
@@ -99,7 +145,11 @@ void fn_destroy_gfx_cmd_buffer(struct fn_gfx_cmd_buffer buffer);
 //==============================================================================
 //                                  BUFFERS
 //
-// Used to store data such as vertices or indices to be used for rendering.
+// Represents vertex-, index- and constants-buffers, which are later bound to
+// pipeline(s).
+//
+// Note:
+// - Constant buffers are also known as uniform buffers.
 //==============================================================================
 
 struct fn_gfx_buffer{ uint32_t device_id; uint32_t id; };
@@ -119,25 +169,31 @@ void fn_destroy_gfx_buffer(struct fn_gfx_buffer buffer);
 //=============================================================================
 //                                 PIPELINES
 //
-// Describe the entire state of the pipeline.
+// Represents the entire state of the pipeline, allowing to validate the pipe-
+// line state upfront.
+//
+// Stores informations such as:
+// - current shader program (vertex-, pixel- and/or compute shader)
+// - currently bound vertex buffer(s) and index buffer
+// - topology type
+// - ...
+//
 //=============================================================================
 
 struct fn_gfx_pipeline{ uint32_t device_id; uint32_t id; };
 
 enum fn_gfx_topology_type {
-    fn_gfx_topology_type_triangle_list
-};
-
-struct fn_gfx_rasterizer_state {
-    int foo;
+    fn_gfx_topology_type_none,
+    fn_gfx_topology_type_point,
+    fn_gfx_topology_type_line,
+    fn_gfx_topology_type_triangle
 };
 
 struct fn_gfx_pipeline_desc {
-    enum fn_gfx_topology_type topology;
-    struct fn_gfx_rasterizer_state rasterizer_state;
-    bool                    is_compute;
-    const char*             label;
-    struct fn_gfx_error*    error;
+    enum fn_gfx_topology_type       topology;
+    bool                            is_compute;
+    const char*                     label;
+    struct fn_gfx_error*            error;
 };
 
 struct fn_gfx_pipeline fn_create_gfx_pipeline(
