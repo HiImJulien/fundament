ce14467bb1f8d5614be832ba5d6fd4f198b21d8d
Julian Kirsch <contact@juliankirsch.me>
2020-11-12 22:39:31 +0100

    After hour after hour, I managed to finally render and blit some text using shm and stb_truetype.

--- window/private/wayland/wayland_decorations.c
+++ window/private/wayland/wayland_decorations.c
@@ -15,7 +15,7 @@ static const uint32_t fn__g_bg_color = 0x2d3436;
 static bool                 fn__g_initialized_font_info = false;
 static unsigned char*       fn__g_font_buffer;
 static stbtt_fontinfo       fn__g_font_info;
-static int                  fn__g_font_height = 50;
+static int                  fn__g_font_height = 25;
 static float                fn__g_font_scaling = 0;
 
 static const char* font_path = "/usr/share/fonts/droid/DroidSansMono.ttf";
@@ -39,7 +39,143 @@ static void fn__g_draw_rectangle(
     for(uint32_t iy = 0; iy < height; ++iy)
         for(uint32_t ix = 0; ix < width; ++ix)
             image_data[(y + iy) * window->width + x + ix] = color;
-        
+}
+
+static uint32_t fn__g_rgba_to_rgb(uint32_t rgb, unsigned char a) {
+    uint8_t* _rgb = (uint8_t*) &rgb; 
+    float b = _rgb[0] / 255.f;
+    float g = _rgb[1] / 255.f;
+    float r = _rgb[2] / 255.f;
+
+    float _a = a / 255.f; 
+    // float _a1 = 1.f - a;
+    float _a1 = a;
+
+    b = _a1 * b + _a * b;
+    g = _a1 * g + _a * g;
+    r = _a1 * r + _a * r;
+    
+    _rgb[0] = roundf(b * 255.f);
+    _rgb[1] = roundf(g * 255.f);
+    _rgb[2] = roundf(r * 255.f);
+
+    rgb = 0;
+    _rgb[0] = a;
+    _rgb[3] = 0;
+
+    return rgb;
+}
+
+static void fn__g_draw_text(
+    fn__window* window,
+    uint32_t*   image_data,
+    uint32_t    x,
+    uint32_t    y,
+    const char* text
+) {
+    const int text_height = fn__g_font_height;
+    int text_width = 0;
+    for(int it = 0; it < strlen(text); ++it) {
+        int width, bearing;
+        stbtt_GetCodepointHMetrics(
+            &fn__g_font_info, 
+            text[it], 
+            &width, 
+            &bearing
+        );
+
+        int kern = stbtt_GetCodepointKernAdvance(
+            &fn__g_font_info,
+            text[it],
+            text[it+1]
+        );
+
+        text_width += roundf(width * fn__g_font_scaling);
+        text_width += roundf(kern * fn__g_font_scaling);
+    }
+
+    int ascent, descent, line_gap;
+    stbtt_GetFontVMetrics(
+        &fn__g_font_info,
+        &ascent,
+        &descent,
+        &line_gap
+    );
+
+    ascent = roundf(ascent * fn__g_font_scaling);
+    descent = roundf(descent * fn__g_font_scaling);
+
+    // VLA causes a segfault for whatever reasons.
+    // unsigned char bitmap[text_width * text_height];
+    // memset(bitmap, 0, text_width * text_height * sizeof(unsigned char));
+    unsigned char* bitmap = calloc(
+        text_width * text_height, 
+        sizeof(unsigned char)
+    );
+
+    int cx = 0;
+    for(int it = 0; it < strlen(text); ++it) {
+        int width, bearing;
+        stbtt_GetCodepointHMetrics(
+            &fn__g_font_info,
+            text[it],
+            &width,
+            &bearing
+        );
+
+        int x1, y1, x2, y2;
+        stbtt_GetCodepointBitmapBox(
+            &fn__g_font_info,
+            text[it],
+            fn__g_font_scaling,
+            fn__g_font_scaling,
+            &x1, &y1,
+            &x2, &y2
+        );
+
+        const int y = ascent + y1;
+        int byte_offset = cx 
+            + roundf(width * fn__g_font_scaling) 
+            + (y * text_width);
+
+        stbtt_MakeCodepointBitmap(
+            &fn__g_font_info,
+            bitmap + byte_offset,
+            x2 - x1,
+            y2 - y1,
+            text_width,
+            fn__g_font_scaling,
+            fn__g_font_scaling,
+            text[it]
+        );
+
+        int kern = stbtt_GetCodepointKernAdvance(
+            &fn__g_font_info,
+            text[it],
+            text[it+1]
+        );
+
+        cx += roundf(width * fn__g_font_scaling);
+        cx += roundf(kern * fn__g_font_scaling);
+    }
+
+    // Time to blit that shit.
+    for(int yt = 0; yt < text_height; ++yt) {
+        for(int xt = 0; xt < text_width; ++xt) {
+            printf("X: %i Y: %i\n", xt, yt);
+            unsigned char pixel = bitmap[yt * text_width + xt];
+
+            if(pixel != 0)
+                printf("%i\n", (int) pixel);
+
+            uint32_t* out_pixel = &image_data[(y + yt) * window->width + x + xt];
+            *out_pixel = fn__g_rgba_to_rgb(fn__g_fg_color, pixel);
+        }
+    }
+
+    printf("W: %i H: %i\n", text_width, text_height);
+
+    free(bitmap);
 }
 
 void fn__init_wayland_decorations() {
@@ -99,6 +235,14 @@ void fn__decorate_wayland_window(fn__window* window, uint32_t* image_data) {
         25, 
         25, 
         fn__g_fg_color
+    );
+
+    fn__g_draw_text(
+        window,
+        image_data,
+        10, 
+        10,
+        window->title
     );
 }
 
